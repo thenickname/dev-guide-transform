5f3ed9c421172f1d03aa969a4ce36d5a2fa2ae94
Nick Mussin <nmussin@eclipsesource.com>
2012-04-10 12:50:01 +0200

    Removed whitespaces

--- FileSystemUtil.php
+++ FileSystemUtil.php
@@ -1,40 +1,40 @@
 <?php
 
 class FileSystemUtil {
-  
+
   const READ_ONLY = 'r';
   const READ_WRITE = 'r+';
   const WRITE_ONLY = 'w';
-  
+
   private function __construct() {}
-  
+
   public static function isDir( $directoryPath ) {
     return is_dir( $directoryPath );
   }
-  
+
   public static function isDirEmpty( $directoryPath ) {
     $iterator = new RecursiveDirectoryIterator( $directoryPath );
     return $iterator -> hasChildren();
   }
-  
+
   public static function createFile( $filePath, $openMode ) {
     $handle = fopen( $filePath, $openMode );
     fclose( $handle );
     return new SplFileObject( $filePath, $openMode );
   }
-  
+
   public static function removeFile( $filePath ) {
     unlink( $filePath );
   }
-  
+
   public static function createDir( $directoryPath ) {
     mkdir( $directoryPath );
   }
-  
+
   public static function removeDir( $directoryPath ) {
     rmdir( $directoryPath );
   }
-  
+
   public static function removeDirRecursively( $directoryPath ) {
     $iterator = new FilesystemIterator( $directoryPath );
     foreach( $iterator as $fileInfo ) {
@@ -44,9 +44,9 @@ class FileSystemUtil {
         self::removeFile( $fileInfo -> getPathname() );
       }
     }
-    self::removeDir( $directoryPath );    
+    self::removeDir( $directoryPath );
   }
-  
+
 }
 
 ?>
--- Main.php
+++ Main.php
@@ -5,11 +5,11 @@ require_once 'FileSystemUtil.php';
 use FileSystemUtil as FS;
 
 class Main {
-  
+
   private static $destinationPath;
-  
+
   private function __construct() {}
-  
+
   public static function run( $sourcePath, $destinationPath, $navXmlFileName ) {
     self::$destinationPath = $destinationPath;
     if( FS::isDir( $destinationPath ) ) {
@@ -19,21 +19,21 @@ class Main {
     self::processNavigationXml( $navXmlFileName );
     self::processHelpDirectory( $sourcePath );
   }
-  
+
   private static function processNavigationXml( $fileName ) {
     $file = FS::createFile( self::$destinationPath . "/navigation.php", FS::WRITE_ONLY );
     $xmlIterator = new SimpleXMLIterator( $fileName, null, true );
     self::createNavigation( $file, $xmlIterator );
   }
-  
+
   private static function createNavigation( $file, $xmlIterator ) {
     $file -> fwrite( "<ul>" );
     foreach( $xmlIterator as $element ) {
       if( $element[ 'href' ] ) {
         $url = str_replace( "help/html/", "developers-guide/", $element[ 'href' ] );
-        $file -> fwrite( "<li><a href='" . $url . "'>" . $element[ 'label' ] . "</a></li>" );        
-      } else {        
-        $file -> fwrite( "<li class='category-group'>" . $element[ 'label' ] . "</li>" );        
+        $file -> fwrite( "<li><a href='" . $url . "'>" . $element[ 'label' ] . "</a></li>" );
+      } else {
+        $file -> fwrite( "<li class='category-group'>" . $element[ 'label' ] . "</li>" );
       }
       if( $element -> count() > 0 ) {
         $file -> fwrite( "<li>" );
@@ -43,7 +43,7 @@ class Main {
     }
     $file -> fwrite( "</ul>" );
   }
-  
+
   private static function processHelpDirectory( $dirPath ) {
     FS::createDir( self::$destinationPath . "/" . $dirPath );
     $iterator = new FilesystemIterator( $dirPath );
@@ -55,14 +55,14 @@ class Main {
       }
     }
   }
-  
+
   private static function handleHtmlFile( $fileInfo ) {
     $fileContent = self::readFileContent( $fileInfo );
     $bodyContent = self::stripHtmlBodyContent( $fileContent );
     $newFilePath = self::$destinationPath . "/" . $fileInfo -> getPathname();
     self::writeInNewFile( $newFilePath, $bodyContent );
   }
-  
+
   private static function readFileContent( $fileInfo ) {
     $result = "";
     $file = $fileInfo -> openFile();
@@ -71,17 +71,17 @@ class Main {
     }
     return $result;
   }
-  
+
   private static function stripHtmlBodyContent( $fileContent ) {
     preg_match("|<body\b[^>]*>(.*?)</body>|s", $fileContent, $matches );
     return $matches[ 1 ];
   }
-  
+
   private static function writeInNewFile( $filePath, $fileContent ) {
     $htmlFile = FS::createFile( $filePath, FS::WRITE_ONLY );
     $htmlFile -> fwrite( $fileContent );
   }
-  
+
 }
 
 ?>
